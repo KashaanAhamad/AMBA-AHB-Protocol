ecf55f2de67efbd774a661bc7b1ade607582ec9a
Kashaan Ahmad <kashaan.bryan@gmail.com>
2026-08-07 23:49:23 +0530

    Creating Data Generation logic Module for Slave 1

---
+++ data_gen_logic_slave_1.sv
@@ -0,0 +1,78 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.07.2026 13:02:47
+// Design Name: 
+// Module Name: data_gen_logic_slave_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module data_gen_logic_slave_1(
+	input logic hclk_s1,
+	input logic hreset_s1,
+	input logic hready_s1,
+	
+	//Signal comming from Address_loader
+	input logic [31:0]addrs_in_s1,
+	
+	input logic [31:0]hwdata_s1,
+	
+	//Signal comming from FSM
+	input logic load_data_s1,
+	input logic store_data_s1,
+	
+	//Signal going to FSM
+	output logic [31:0]data_out_s1
+    );
+    
+    parameter reg [31:0]data_memory_s1 =32'h1A5b203b;
+    
+    reg [31:0]data_mem_s1;
+    reg [7:0] slave_mem_s1[0:1023];
+    
+    always_comb
+    begin
+    	if(!hreset_s1)
+    		begin
+    			data_mem_s1 = data_memory_s1;
+    			for(int i=512; i<1024; i=i+4)begin
+    			slave_mem_s1[i] = data_mem_s1[31:24];
+    			slave_mem_s1[i+1] = data_mem_s1[23:16];
+    			slave_mem_s1[i+2] = data_mem_s1[15:8];
+    			slave_mem_s1[i+3] = data_mem_s1[7:0];
+    			data_mem_s1 = data_mem_s1 +4;
+    			end
+    		end 
+    		else if(store_data_s1 == 1)
+    		begin
+    			slave_mem_s1[address_in_s1]<= hwdata_s1[31:24];
+    			slave_mem_s1[address_in_s1+1]<= hwdata_s1[23:16];
+    			slave_mem_s1[address_in_s1+2]<= hwdata_s1[15:8];
+    			slave_mem_s1[address_in_s1+3]<= hwdata_s1[7:0];
+    		end
+    	end
+    	
+    always @(posedge hclk_s1) 
+    begin
+    	if(load_data_s1) begin
+    		if(hready_s1)
+    			data_out_s1 <= {slave_mem_s1[address_in_s1],
+    							slave_mem_s1[address_in_s1+1],
+    							slave_mem_s1[address_in_s1+2],
+    							slave_mem_s1[address_in_s1+3]};
+    	end
+    end
+endmodule

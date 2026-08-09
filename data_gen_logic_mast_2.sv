df34df65d7f24e857ec02a07c624b59999042c2c
Kashaan Ahmad <kashaan.bryan@gmail.com>
2026-08-09 23:50:23 +0530

    [Fix] The Reset condition is fixed

--- data_gen_logic_mast_2.sv
+++ data_gen_logic_mast_2.sv
@@ -27,13 +27,13 @@ module data_gen_logic_mast_2(
 	input wire hready_m2,
 	input wire load_out_m2,
 	
-	output reg data_out_m2
+	output reg [31:0]data_out_m2
     );
-    parameter reg[31:0] data_m2=32'h01a7_d34c;//change value here
+    parameter reg[31:0] data_m2=32'hffff_d34c;
     //Next data Generation Logic to write in slave
     always @(posedge hclk_m2,negedge hreset_m2)
     	begin
-    	  if(hreset_m2)
+    	  if(!hreset_m2)
     	    data_out_m2<=data_m2;
     	  else if(load_out_m2 && hready_m2)
     	    data_out_m2 <= data_out_m2+32'h0000_0010; //might need to change the value.

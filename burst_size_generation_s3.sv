d8edea1572f69da294b1baa2f56f0d7c7142fa72
Kashaan Ahmad <kashaan.bryan@gmail.com>
2026-08-13 23:50:56 +0530

    Creating Burst Size Generation Module for slave 3

---
+++ burst_size_generation_s3.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.07.2026 22:57:28
+// Design Name: 
+// Module Name: burst_size_generation_s3
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module burst_size_generation_s3(
+
+    );
+endmodule
